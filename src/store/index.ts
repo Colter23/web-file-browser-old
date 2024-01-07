5ff586e91a62d8ec017f14a1eb529e8c842c9c8f
zhengyi <admin@mczhengyi.top>
2024-01-07 14:37:24 +0800

    refactor: remove mode and session when close tab

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import {EditFileInfo, EditorSession} from "../class.ts";
 
 
 export const useEditorStore = defineStore('editor', {
-    state: () => ({
+    state: (): PiniaState => ({
         // 当前编辑文件路径
         currentFile: '' as string,
         // 编辑文件信息列表  key为文件路径
@@ -41,6 +41,9 @@ export const useEditorStore = defineStore('editor', {
         },
         // 删除编辑文件
         deleteEditFile(key: string): boolean {
+            const deleteItem = this.editFileList.get(key);
+            delete deleteItem.mode;
+            delete deleteItem.session;
             this.editFileList.delete(key);
             return true
         },
@@ -48,3 +51,7 @@ export const useEditorStore = defineStore('editor', {
     }
 })
 
+declare type PiniaState = {
+    editFileList: Map<string, EditFileInfo>;
+    currentFile: string;
+}
